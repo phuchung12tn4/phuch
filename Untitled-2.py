1038fd70cdb49b87cde26d9a38e3d2e4a626a2b0
phuchung12tn4 <phuchung12tn4@gmail.com>
2024-01-11 19:35:03 +0700

    abc

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,1 +1,7 @@
-print("hungpro")
+Nhiet_do = input("Nhập nhiệt độ các ngày trong tuần theo thứ tự: ")
+list_nhietdo = Nhiet_do.split(",")
+Ngay_trong_tuan = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+b = -1
+for a in list_nhietdo:
+    b = b + 1 
+    print(f'{Ngay_trong_tuan[b]} : {a}')
